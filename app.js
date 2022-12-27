981d82ec5ff83594849bb99b989ef0d1b1e37fdb
TarıkSoğukpınar <hanluee@yandex.com.tr>
2022-12-27 10:03:53 +0300

    ui refactoring & postgre req is deleted

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const postgreClient = require("./helpers/postgreClient");
 const dotenv = require("dotenv");
 const indexRoutes = require("./router/indexRoutes");
 const contactRoutes = require("./router/contactRoutes");
-const adminRoutes = require("./router/adminRoutes");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,13 +14,7 @@ app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
-  // postgreClient.connect((error) => {
-  //   if (error) {
-  //     console.log(`Error Connecting To Postgres ${error}`);
-  //   } else {
-  //     console.log("Postgre Connection Successful!");
-  //   }
-  // });
+ 
 });
 
 app.use(express.static(__dirname + "/public"));
@@ -30,12 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+
+
 app.use("/", indexRoutes);
 app.use("/contact", contactRoutes);
 app.use("/success", contactRoutes);
-app.use("/admin", adminRoutes);
 
-app.use((req, res) => {
-  res.render("404", { PageTitle: "Page Doesn't Exist" });
-  res.status(404);
-});
+
+// app.use((req, res) => {
+//   res.render("404", { PageTitle: "Page Doesn't Exist" });
+//   res.status(404);
+// });
--- helpers/postgreClient.js
+++ helpers/postgreClient.js
@@ -1,9 +1,9 @@
-const pg = require("pg");
-const dotenv = require("dotenv");
-dotenv.config();
+// const pg = require("pg");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const postgreClient = new pg.Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-});
+// const postgreClient = new pg.Pool({
+//   connectionString: process.env.DB_CONNECTION_STRING,
+// });
 
-module.exports = postgreClient;
+// module.exports = postgreClient;
