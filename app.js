487084a4fb7e88702daeaa76adab5fba5a34ac89
TarıkSoğukpınar <hanluee@yandex.com.tr>
2023-01-12 16:48:49 +0300

    index.ejs clean up

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 
-// app.use((req, res) => {
-//   res.render("404", { PageTitle: "Page Doesn't Exist" });
-//   res.status(404);
-// });
+app.use((req, res) => {
+  res.render("404", { PageTitle: "Page Doesn't Exist" });
+  res.status(404);
+});
